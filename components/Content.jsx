4401560061003bfc22ce13937dca67745069c352
Auto Logon <autologon@CSED.LAB>
2023-03-14 19:27:07 -0300

    v1.02

--- components/Content.jsx
+++ components/Content.jsx
@@ -88,108 +88,108 @@ export default function Content() {
 
   return (
     <>
-      {/* TOAST */}
+    {/* TOAST */}
 
-      <Toaster position="top-center" reverseOrder={false} />
+    <Toaster position="top-center" reverseOrder={false} />
 
-      {/* SEARCH */}
+    {/* SEARCH */}
 
-      <div className="flex flex-col justify-center items-center gap-3">
-        <h1 className="font-semibold text-sm uppercase">
-          Confira o clima de uma cidade:
+    <div className="flex flex-col justify-center items-center gap-3">
+      <h1 className="font-semibold text-sm uppercase text-white">
+        Confira o clima de uma cidade:
         </h1>
 
-        <div className="relative">
-          <Image
-            src={srcIcon}
-            className="absolute right-[5px] top-[50%] translate-y-[-50%] w-10 cursor-pointer"
-            alt="Procurar"
-            onClick={handleClick}
-            disable={disable}
-          />
-          <input
-            type="text"
-            className=" w-[90vw] sm:w-[500px] px-4 py-2 rounded-[60px] border-2 border-secondary bg-primary text-white "
-            placeholder="Tokyo"
-            onChange={({ target }) => SetCity(target.value)}
-            value={city}
-          />
-        </div>
+      <div className="relative">
+        <Image
+          src={srcIcon}
+          className="absolute right-[5px] top-[50%] translate-y-[-50%] w-10 cursor-pointer"
+          alt="Procurar"
+          onClick={handleClick}
+          disable={disable}
+        />
+        <input
+          type="text"
+          className=" w-[90vw] sm:w-[500px] px-4 py-2 rounded-[60px] border-2 border-secondary bg-primary text-white "
+          placeholder="Tokyo"
+          onChange={({ target }) => SetCity(target.value)}
+          value={city}
+        />
       </div>
+    </div>
 
-      {/* WEATHER INFORMATION */}
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex items-center justify-center gap-2 bg-primary translate-y-6 p-2">
-          <h1 className="font-bold text-2xl"> {cityName} </h1>
-          {flagIco ? (
-            <Image
-              src={flag}
-              width={25}
-              height={25}
-              draggable="false"
-              alt="País"
-            />
-          ) : (
+    {/* WEATHER INFORMATION */}
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center gap-2 bg-primary translate-y-6 p-2">
+        <h1 className="font-bold text-2xl text-white"> {cityName} </h1>
+        {flagIco ? (
+          <Image
+            src={flag}
+            width={25}
+            height={25}
+            draggable="false"
+            alt="País"
+          />
+        ) : (
             ""
           )}
-        </div>
+      </div>
 
-        <div className="flex flex-col items-center justify-center border-2 border-secondary rounded-[30px] w-[80vw] sm:w-[400px] py-3 gap-4">
-          <div className="flex flex-col items-center justify-center">
-            {iconTemp ? (
-              <Image
-                src={imageIconTemp}
-                width={45}
-                height={45}
-                draggable="false"
-                alt="Condição do Tempo"
-              />
-            ) : (
+      <div className="flex flex-col items-center justify-center border-2 border-secondary rounded-[30px] w-[80vw] sm:w-[400px] py-3 gap-4">
+        <div className="flex flex-col items-center justify-center">
+          {iconTemp ? (
+            <Image
+              src={imageIconTemp}
+              width={45}
+              height={45}
+              draggable="false"
+              alt="Condição do Tempo"
+            />
+          ) : (
               ""
             )}
 
-            <h1 className="font-[600] text-[15px] text-secondary">
-              {" "}
-              {description}{" "}
-            </h1>
-          </div>
+          <h1 className="font-[600] text-[15px] text-secondary">
+            {" "}
+            {description}{" "}
+          </h1>
+        </div>
 
-          <div className="flex justify-center items-center gap-5">
-            <div className="flex flex-col justify-center items-center">
-              <span className="font-[500] text-xs uppercase">agora</span>
-              <span className="font-bold text-2xl text-secondary">
-                {tempNow} °C
+        <div className="flex justify-center items-center gap-5">
+          <div className="flex flex-col justify-center items-center">
+            <span className="font-[500] text-xs uppercase text-white">agora</span>
+            <span className="font-bold text-2xl text-secondary">
+              {tempNow} °C
               </span>
-            </div>
-          </div>
-
-          <div className="flex justify-center items-center gap-5">
-            <div className="flex flex-col justify-center items-center">
-              <span className="font-[500] text-xs uppercase">min</span>
-              <span className="font-bold text-base text-secondary">
-                {tempMin} °C
-              </span>
-            </div>
-
-            <div className="h-[30px] border-l-2 border-l-white" />
-
-            <div className="flex flex-col justify-center items-center">
-              <span className="font-[500] text-xs uppercase">max</span>
-              <span className="font-bold text-base text-secondary">
-                {tempMax} °C
-              </span>
-            </div>
-          </div>
-
-          <div className="flex items-center justify-center gap-1">
-            <Image src={drop} alt="chuva porcentagem" draggable="false" />
-            <span className="text-base">
-              {humidity}
-              <span className="text-secondary">%</span>
-            </span>
           </div>
         </div>
+
+        <div className="flex justify-center items-center gap-5">
+          <div className="flex flex-col justify-center items-center">
+            <span className="font-[500] text-xs uppercase text-white">min</span>
+            <span className="font-bold text-base text-secondary">
+              {tempMin} °C
+              </span>
+          </div>
+
+          <div className="h-[30px] border-l-2 border-l-white" />
+
+          <div className="flex flex-col justify-center items-center">
+            <span className="font-[500] text-xs uppercase text-white">max</span>
+            <span className="font-bold text-base text-secondary">
+              {tempMax} °C
+              </span>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center gap-1">
+          <Image src={drop} alt="chuva porcentagem" draggable="false" />
+          <span className="text-base text-white">
+            {humidity}
+            <span className="text-secondary">%</span>
+          </span>
+        </div>
       </div>
+    </div>
     </>
   );
 }
